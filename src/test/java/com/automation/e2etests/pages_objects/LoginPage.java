e1d9d98bebf4194ffca15a296fe2858a4649d945
BilelNaili <neilibileltn@gmail.com>
2025-07-28 22:10:43 +0100

    login feature

--- src/test/java/com/automation/e2etests/pages_objects/LoginPage.java
+++ src/test/java/com/automation/e2etests/pages_objects/LoginPage.java
@@ -1,11 +1,11 @@
 package com.automation.e2etests.pages_objects;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 import com.automation.e2etests.utils.BasePage;
+import com.automation.e2etests.utils.Setup;
 
 public class LoginPage extends BasePage {
 
@@ -17,9 +17,12 @@ public class LoginPage extends BasePage {
 
 	@FindBy(how = How.XPATH, using = "//button[@type='submit']")
 	private static WebElement btnLogin;
+	
+	@FindBy(how = How.XPATH, using = "//h6[normalize-space()='Dashboard']")
+	private static WebElement textDashboard;
 
-	public LoginPage(WebDriver driver) {
-		super(driver);
+	public LoginPage() {
+		super(Setup.getDriver());
 
 	}
 
@@ -38,6 +41,12 @@ public class LoginPage extends BasePage {
 	public static WebElement getBtnLogin() {
 
 		return btnLogin;
+
+	}
+	
+	public static WebElement getTextDashboard() {
+
+		return textDashboard;
 
 	}
 
